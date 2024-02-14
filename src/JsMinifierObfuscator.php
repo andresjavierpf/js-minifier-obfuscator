f42fb60b6428c98650f3e4ae7ab79dc12a0c5b0e
Andrés J. Pavón Fernández <ajavier900710@gmail.com>
2024-02-14 18:04:54 -0500

    Update JsMinifierObfuscator

--- src/JsMinifierObfuscator.php
+++ src/JsMinifierObfuscator.php
@@ -14,11 +14,14 @@ class JsMinifierObfuscator
     public static function minifyAndObfuscate(Event $event) {
         $composer   = $event->getComposer();
         $extra      = $composer->getPackage()->getExtra();
+
+        $event->getIO()->write('Minify and Obfuscate: Initiating library');
         
         $file_paths = $extra['js-minifier-obfuscator'] ?? [];
 
         foreach ($file_paths as $file_path) {
             if (file_exists($file_path)) {
+                $event->getIO()->write(sprintf('Minify and Obfuscate: Processing the file %s', $file_path));
                 $file_js_content = file_get_contents($file_path);
 
                 // Minify
@@ -29,7 +32,12 @@ class JsMinifierObfuscator
                 $file_js_content = $hunter->Obfuscate();
 
                 file_put_contents($file_path, $file_js_content);
+                $event->getIO()->write(sprintf('Minify and Obfuscate: End for file %s', $file_path));
+            } else {
+                $event->getIO()->write(sprintf('Minify and Obfuscate: Not found the file %s', $file_path));
             }
         }
+
+        $event->getIO()->write('Minify and Obfuscate: THE END');
     }
 }
